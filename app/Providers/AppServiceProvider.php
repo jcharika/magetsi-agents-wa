70eabd51edd705bf25953979f0d1376fb1d649dd
Joseph Chareka <joecharika@gmail.com>
2026-06-01 15:55:25 +0200

    enforce HTTPS for non-local environments
    - Add `configureHttps` method to `AppServiceProvider` to call `URL::forceHttps()` when the application environment is not `local`.
    - Update the `register` method to invoke the new HTTPS configuration logic.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->configureHttps();
     }
 
     /**
@@ -20,5 +21,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    }
+
+    private function configureHttps(): void
+    {
+        if ($this->app->environment('local')) {
+            return;
+        }
+
+        URL::forceHttps();
     }
 }
